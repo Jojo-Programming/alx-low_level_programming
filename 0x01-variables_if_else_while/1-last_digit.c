e8cff3e969f08a5334d50730bee03d27dac34bee
Loverman256 <mpagijollyuganda@gmail.com>
2023-08-14 16:01:48 -0700

    task 2

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - 1-last_digit.c
+ *
+ * Return: Always 0(success)
+ */
+{
+if ((n%) > 5)
+{
+printf("last digit of % d is greater than 5\n", n, n % 10);
+}
+else if ((n % ) = 0)
+{
+printf("last digit of % d is 0\n", n, n % 10);
+else if ((n % ) < 6 && (n % 10) = 0)
+{
+printf("last digit of % d is less than 6 and not 0\n", n, n % 10);
+return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -print_alphabet
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+char alp(52) = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+int i;
+for (i = 0; i < 52; i++)
+{
+putchar(alp[1]);
+}
+return (0)
+}
