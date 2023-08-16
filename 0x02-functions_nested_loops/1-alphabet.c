f117543421f4af75bb79fa7a2654cacbc17007a0
Loverman256 <mpagijollyuganda@gmail.com>
2023-08-16 04:49:22 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include <main.h>
+/**
+ * print_alphabets 10 times
+ * written by Mpagi Jolly
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+	char letter;
+	for (letter = 'a'; letter <= '2'; letter++)
+	_putchar(letter);
+	_putchar('\n');
