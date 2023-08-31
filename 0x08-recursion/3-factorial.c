dc9d7488d56fd67a875bc278f4581fd117e9e6b3
Loverman256 <mpagijollyuganda@gmail.com>
2023-08-31 10:43:35 -0700

    taska

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a given number
+ * @n: integer
+ * By:Mpagi Jolly/Loverman256
+ * Return: always 0 success
+ */
+
+int factorial(int n)
+{
+if (n < 0)
+return (-1);
+if (n == 0)
+return (1);
+return (n * factorial(n - 1));
+}
